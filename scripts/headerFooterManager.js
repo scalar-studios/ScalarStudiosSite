2d484aa8d5266c2d98fc7e2432adaadc12c68619
Robert <47193272+Lemon-Juiced@users.noreply.github.com>
2025-07-30 09:24:27 -0400

    Update headerFooterManager.js

--- scripts/headerFooterManager.js
+++ scripts/headerFooterManager.js
@@ -13,4 +13,20 @@ class CustomNavBar extends HTMLElement {
     }
 }
 
+class CustomNavBarIndented extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = 
+        `
+        <ul class="navbar">
+            <li><a href="../index.html">Home</a></li>
+            <li><a href="../projects.html">Projects</a></li>
+            <li><a href="../social_media.html">Social Media</a></li>
+            <li><a href="../wikis.html">Wikis</a></li>
+            <li><a href="../external_pages.html">External Pages</a></li>
+        </ul>
+        `
+    }
+}
+
 customElements.define('custom-navbar', CustomNavBar)
+customElements.define('custom-navbar-indented', CustomNavBarIndented);
