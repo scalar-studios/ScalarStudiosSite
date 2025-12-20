d370e97c4476f0f4cfcd3df8045695b8de7483dd
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2025-12-20 03:11:22 -0500

    Added new Crusader Kings Mods Section

--- scripts/headerFooterManager.js
+++ scripts/headerFooterManager.js
@@ -5,7 +5,12 @@ class CustomNavBar extends HTMLElement {
         <ul class="navbar">
             <li><a href="index.html">Home</a></li>
             <li><a href="projects.html">Projects</a></li>
-            <li><a href="modjams.html">Mod Jams</a></li>
+            <li class="dropdown">
+                <a href="mods.html" class="dropbtn">Mods</a>
+                <ul class="dropdown-content">
+                    <li><a href="modjams.html">Mod Jams</a></li>
+                </ul>
+            </li>
             <li><a href="social_media.html">Social Media</a></li>
             <li><a href="wikis.html">Wikis</a></li>
             <li><a href="external_pages.html">External Pages</a></li>
@@ -21,7 +26,12 @@ class CustomNavBarIndented extends HTMLElement {
         <ul class="navbar">
             <li><a href="../index.html">Home</a></li>
             <li><a href="../projects.html">Projects</a></li>
-            <li><a href="../modjams.html">Mod Jams</a></li>
+            <li class="dropdown">
+                <a href="../mods.html" class="dropbtn">Mods</a>
+                <ul class="dropdown-content">
+                    <li><a href="../modjams.html">Mod Jams</a></li>
+                </ul>
+            </li>
             <li><a href="../social_media.html">Social Media</a></li>
             <li><a href="../wikis.html">Wikis</a></li>
             <li><a href="../external_pages.html">External Pages</a></li>
